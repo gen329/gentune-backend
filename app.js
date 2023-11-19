1267d1bc47e9659ceafd4db4d3988025dd9d7490
Genesis Lara <genesislara@pursuit.org>
2023-11-19 12:20:18 -0500

    completes app.js file

---
+++ app.js
@@ -0,0 +1,18 @@
+const express = require("express");
+const cors = require("cors");
+const app = express();
+const songsController = require("./controllers/songsController");
+
+app.use(cors());
+app.use(express.json());
+app.use("/songs", songsController);
+
+app.get("/", (req,res) => {
+  res.send("WELCOME TO GENTUNE!!!")
+});
+
+app.get("*", (req,res) => {
+  res.status(404).json({ sucess: false, data: {error: "page not found"}});
+});
+
+module.exports = app;
