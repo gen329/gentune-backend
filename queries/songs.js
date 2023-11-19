794cc685d2c7381534f3fe37ffb653b198ca4516
Genesis Lara <genesislara@pursuit.org>
2023-11-19 17:36:38 -0500

    works on song querie

---
+++ queries/songs.js
@@ -0,0 +1,58 @@
+const db = require("../db/dbCongif");
+
+const getAllSongs = async () => {
+  try {
+    const allSongs = await db.any("SELECT * FROM songs");
+    return allSongs
+  } catch(error) {
+    return { error: "Songs not found!" };
+  }
+};
+
+const getOneSong = async (id) => {
+  try {
+    const oneSong = await db.one("SELECT * FROM songs WHERE id=$1", id)
+    return oneSong
+  } catch(error) {
+    return { error: "Song not found!" };
+  }
+};
+
+const createSong = async (song) => {
+  try {
+    const createdSong = await db.one(
+      "INSERT INTO songs (title, artist_name, album, year_of_release, runtime, is_favorite) VALUES ($1, $2, $3, $4, $5, $6) RETURNING *",
+      [song.title, song.artist, song.album, song.year, song.runtime, song.is_favorite]
+    );
+    return createdSong;
+  } catch (error) {
+    return { error: "Song not created!" };
+  }
+};
+
+const deleteSong = async (id) => {
+  try {
+    const deletedSong = await db.one("DELETE from movies WHERE id =$1 RETURNING *", id)
+    return deletedSong
+  } catch(error) {
+    return { error: "Song not deleted!"}
+  }
+};
+
+const updatedSong = async (id,song) => {
+  try {
+    const { title, artist_name, album, year_of_release, runtime, is_favorite } = song;
+    const updatedSong = await db.one("UPDATE song SET title=$1, artist_name=$2, album=$3, year_of_release=$4, runtime=$5, is_favorite=$6, WHERE id=$7", [title,artist_name,album,year_of_release,runtime,is_favorite]);
+    return updatedSong
+  } catch(error) {
+    return { error: "Song not updated!" }
+  }
+};
+
+module.exports = {
+  getAllSongs,
+  getOneSong,
+  createSong,
+  deleteSong,
+  updatedSong
+}
