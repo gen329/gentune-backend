e3c211ff600faf930d53523096e1ffa60fa7fbd0
Genesis Lara <genesislara@pursuit.org>
2023-11-30 20:41:40 -0500

    removes duplicate db

--- db/dbCongif.js
+++ db/dbCongif.js
@@ -4,8 +4,9 @@ require('dotenv').config();
 const cn = {
   host: process.env.PG_HOST,
   port: process.env.PG_PORT,
-  database: process.env.PG_DATABASE,
-  user: process.env.PG_USER
+  user: process.env.PG_USER,
+  password: process.env.LOCAL_PASSWORD || 'j7YIQgNuar8eNTNSV9sglFrCdkvgjwyk',
+  database: process.env.LOCAL_DB || 'postgres://gen329:j7YIQgNuar8eNTNSV9sglFrCdkvgjwyk@dpg-clkjaqeaov6s73880eq0-a/song_dev'
 };
 
 const db = pgp(cn);
